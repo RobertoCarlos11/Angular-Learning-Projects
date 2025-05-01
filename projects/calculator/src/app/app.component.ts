dd5efe9a9ccc3d487663234baba6cfabfc45b5bd
Roberto Carlos <roberto.dominguezesp@uanl.edu.mx>
2025-04-30 18:49:48 -0600

    feat: Refactor calculator app with Angular Material components
    - Updated app.component.html to implement Material Design with mat-card and mat-button for a better UI experience.
    - Enhanced app.component.ts to handle operator and number clicks, evaluate expressions, and manage state for input and results.
    - Configured app.config.ts to include animations for a smoother user experience.
    - Modified index.html to include Google Fonts and Material Icons for improved aesthetics.
    - Adjusted main.ts for proper application bootstrapping.
    - Added global styles in styles.css for consistent typography and layout.

--- projects/calculator/src/app/app.component.ts
+++ projects/calculator/src/app/app.component.ts
@@ -1,12 +1,59 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import {MatButtonModule} from '@angular/material/button';
+import {MatCardModule} from '@angular/material/card';
+import { NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [MatButtonModule, MatCardModule, NgIf],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'Calculator';
+  title = 'Calculator'
+  operators: string[] = ['+', '-', '*', '/'];
+  numbers: number[] = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
+  result: number = 0;
+  input: string = "";
+
+  onOperatorClick(operator: string) {
+    if(this.input === "" && this.result === 0) return;
+
+    if(this.operators.includes(this.input[this.input.length - 1])) {
+      this.input = this.input.slice(0, -1);
+    }
+
+    if(this.result !== 0 && this.input === "")
+    {
+      this.input = this.result.toString();
+    }
+    this.input += operator;
+
+  };
+
+  onEqualClick() : void {
+    if(this.input === "" && this.operators.includes(this.input[this.input.length - 1])) return;
+    this.result = this.evaluateCurrentExpression();
+    this.input = "";
+  };
+
+  evaluateCurrentExpression() : number
+  {
+    const func = new Function('return ' + this.input);
+    try {
+      return func();
+    } catch (error) {
+      console.error("Error evaluating expression:", error);
+      return 0;
+    }
+  }  
+
+  onClearClick() {
+    this.input = "";
+    this.result = 0;
+  };
+
+  onNumberClick(number: number) {
+    this.input += number.toString();
+  };
 }
